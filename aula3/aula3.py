7d3640dac2562035383667735168305f72980605
Bruno A. Santana <brunosantana87@gmail.com>
2022-07-01 00:04:31 -0300

    Fim da aula 3.

---
+++ aula3/aula3.py
@@ -0,0 +1,17 @@
+'''
+String é um tipo de dado primitivo em Pyhton e que possui várias funções
+Tudo em Python é um objeto, incluindo os tipos primitivos
+str - string
+String é uma cadeia de caracteres dentro de aspas
+'''
+
+print('alguma coisa')
+print("Aspas duplas")
+print('123456')
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Esse é meu \"texto\" (str).")
+print('Esse é meu \'texto\' (str).')  # O problema é que o uso de "\" deixa o texto feio
+print('Esse é meu \n (str).')  # Nesse caso o \n será lido como quebra linha
+print(r'Esse é meu \n (str).')  # Essa é uma forma da "\" ser lida no texto. O r vem de raw.
+print('\nFim da aula 3!')
