910de00195aa8aa8684147314a35ab35708e01ba
Bruno A. Santana <brunosantana87@gmail.com>
2022-06-30 22:46:55 -0300

    Initial commit

---
+++ aula1/aula1.py
@@ -0,0 +1,25 @@
+print('Hello world! Olá mundo!')
+
+# Isso é um comentário
+
+print('Linha 1')
+print('Linha 2')
+# Comentário sobre a linha 2
+# outra linha
+print('Linha 3')
+print('Linha 4')  # Isso é um outro comentário
+print('Linha 5')
+print('Linha 6')
+# print('Linha 7')
+# Final
+
+"""
+Isso é para documentação, mas também é utilizado como comentário.
+Cria um bloco para poder comentar.
+"""
+
+'''
+Pode ser utilizado tanto as aspas duplas como as aspas simples.
+'''
+
+print('Meu nome é Bruno')  # Outro comentário
---
+++ aula2/aula2.py
@@ -0,0 +1,19 @@
+'''
+# print(123456)
+# print('Bruno', 'Santana')
+
+print('Bruno', 'Santana', sep='-', end='#####')
+print('João', 'e', 'Maria', sep='-', end='')
+
+sep: utilizado como separador na função print
+end: utilizado ao final. Pode ser utilizado para evitar a quebra de linha automática, ligando 2 ou mais funções print.
+'''
+
+'''
+931.959.180-64
+cpf aleatório para exercício envolvendo sep e end
+'''
+print('931', '959', '180', sep='.', end='-')
+print('64')
+
+# E essa foi a aula 2.
